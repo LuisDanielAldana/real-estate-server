bc6f71e0b9a968ef828d24360bce2bf71bc02d59
Luis Daniel Aldana de la Rosa <ldaldana001@gmail.com>
2022-11-14 19:49:10 -0600

    Changed find from get to post

--- routes/houses.js
+++ routes/houses.js
@@ -10,7 +10,7 @@ router.get('/', function(req, res, next) {
 
 router.post('/create', houseController.createHouse);
 
-router.get('/find',houseController.findHouses);
+router.post('/find',houseController.findHouses);
 
 router.get('/favorites',houseController.findFavorites)
 
