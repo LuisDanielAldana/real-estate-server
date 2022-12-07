78159b83fdb65c9252ac4159f3a2aed613009284
Luis Daniel Aldana de la Rosa <ldaldana001@gmail.com>
2022-12-06 18:28:40 -0600

    Updated cloudinary api keys

--- utils/cloudinary.js
+++ utils/cloudinary.js
@@ -1,8 +1,8 @@
 const cloudinary = require('cloudinary').v2
 cloudinary.config({
-    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-    api_key: process.env.CLOUDINARY_API_KEY,
-    api_secret: process.env.CLOUDINARY_API_SECRET
+    cloud_name: 'dficrwc6r',
+    api_key: '112142463854454',
+    api_secret: 'SlXER778yaJOq4t99f3t04gg5jA'
 });
 
 module.exports = cloudinary
